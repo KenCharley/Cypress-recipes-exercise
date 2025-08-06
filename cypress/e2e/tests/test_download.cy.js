3392930a31e7820a81747625123a1e865d4f0c6f
PeigeZhu <peige.zhu.ext@siemens-healthineers.com>
2025-08-06 17:47:54 +0800

    merge lastest to main

--- cypress/e2e/tests/test_download.cy.js
+++ cypress/e2e/tests/test_download.cy.js
@@ -1,3 +1,14 @@
 import { DownloadPage } from "../pages/download.cy";
 
-describe
+describe('Download Test', () => {
+    const downloadPage = new DownloadPage();
+
+    it('Should download and save in local successfully', () => {
+        cy.visit('http://localhost:8070/');
+        cy.get('body').should('contain','The first group of examples uses ' )
+
+        downloadPage.firstCSV.should('exist').click()
+
+
+    })
+})
